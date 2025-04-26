2b15c6bf3518baaa86c5b0f2ead72f01e66f98ea
Kelvin Zhang <bojunzhang1@gmail.com>
2025-04-25 20:01:30 -0500

    updated profile.py

--- profile.py
+++ profile.py
@@ -1,4 +1,3 @@
-# profile.py
 # -*- coding: utf-8 -*-
 import geni.portal as portal
 import geni.rspec.pg as rspec
@@ -8,47 +7,31 @@ NUM_NODES = 4
 pc = portal.Context()
 request = rspec.Request()
 
-# Create a private LAN for all replicas
 lan = request.LAN('lan0')
 lan.best_effort = False
 
 for i in range(NUM_NODES):
-    # Name them node0, node1, ...
-    node = request.RawPC(f'node{i}')
-    iface = node.addInterface(f'if{i}')
+    name = "node{}".format(i)
+    node = request.RawPC(name)
+    iface = node.addInterface("if{}".format(i))
     lan.addInterface(iface)
 
-    # Bootstrap script: install Tendermint, init a home, show node ID
-    node.addService(rspec.Execute(
-        shell='bash',
-        command=f"""#!/bin/bash
+    cmd = """
+#!/bin/bash
 set -e
 
-# 1) Quick-install Go & Tendermint
 curl -L https://git.io/fFfOR | bash
 source ~/.profile
 
-# 2) Prepare a private TM home for this replica
-TMHOME=/local/node{i}
+TMHOME=/local/node{0}
 mkdir -p $TMHOME
-
-# 3) Initialize Tendermint (generates config & genesis)
 tendermint init --home $TMHOME
 
-# 4) Print out this node's ID and IP so you can assemble persistent_peers
 NODE_ID=$(tendermint show_node_id --home $TMHOME)
 IP=$(hostname -I | awk '{{print $1}}')
-echo ">>> node{i} ID=$NODE_ID at $IP:26656 <<<"
+echo ">>> {1} ID=$NODE_ID at $IP:26656 <<<"
+""".format(i, name)
 
-# 5) (optional) start Tendermint with a placeholder peer list
-#    Replace the PERSISTENT_PEERS below with the comma-separated list
-#    you collect from the four nodes' echo lines above.
-# tendermint node \
-#   --home $TMHOME \
-#   --proxy_app=kvstore \
-#   --p2p.laddr tcp://0.0.0.0:26656 \
-#   --p2p.persistent_peers="PERSISTENT_PEERS"
-"""
-    ))
+    node.addService(rspec.Execute(shell="bash", command=cmd))
 
-portal.Context().printRequestRSpec(request)
+pc.printRequestRSpec(request)
